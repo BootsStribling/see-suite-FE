0961d97eda8bbfeb7133d5f0b0fd2f16e64c06be
najeesimmons <najee.bryant.simmons@gmail.com>
2022-05-04 15:39:13 -0400

    style edits for active link colors

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,29 +1,39 @@
-import { Link, Routes, Route } from 'react-router-dom'
-import "./NavBar.css"
-import Sales from '../Sales/Sales'
+import { Link, Routes, Route, useLocation } from "react-router-dom";
+import "./NavBar.css";
+import Sales from "../Sales/Sales";
 import Dashboard from "../Dashboard/Dashboard";
 
 const NavBar = () => {
+  const location = useLocation()
+  const renderNavLink = (to, label) => {
+    const isActiveRoute = to === location.pathname;
+    const className = isActiveRoute ? "text-link active" : "text-link";
+    return (
+      <li className={className}>
+        <Link to={to}>{label}</Link>
+      </li>
+    );
+  };
+
   return (
     <>
-        <nav>
-          <div>
-            <li className='greeting'>Hello Stakeholder!</li>
-          </div>
-          <ul>
-            <li><Link className='text-link' to="/sales" style={{ textDecoration: 'none' }}>Sales</Link></li>
-            <li><Link className='text-link' to="" style={{ textDecoration: 'none' }}>Loan Payments</Link></li>
-            <li><Link className='text-link' to="" style={{ textDecoration: 'none' }}>Inventory</Link></li>
-            <li><Link className='text-link' to="" >Commission</Link></li>
-          </ul>
-        </nav>
-        <Routes>
-          <Route path="/" exact element={ <Dashboard />} />
-          <Route path="/sales" element={ <Sales />} />
-        </Routes>
-    
+      <nav>
+        <div>
+          <li className="greeting">Hello Stakeholder!</li>
+        </div>
+        <ul>
+          {renderNavLink("/sales", "Sales")}
+          {renderNavLink("", "Loan Payments")}
+          {renderNavLink("", "Inventory")}
+          {renderNavLink("", "Commisions")}
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" exact element={<Dashboard />} />
+        <Route path="/sales" element={<Sales />} />
+      </Routes>
     </>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;

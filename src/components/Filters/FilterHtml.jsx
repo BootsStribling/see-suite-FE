8137e3a0d4ddac359a4eb38f91bbb32d39bb5691
BootsStribling <williamstriblingenterprises@gmail.com>
2022-05-05 15:59:22 -0500

    filter styling

--- src/components/Filters/FilterHtml.jsx
+++ src/components/Filters/FilterHtml.jsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
 import FilterItems from "./FilterItems";
-import css from './Filters.css'
-import css from './FilterHtml.css'
+import './FilterHtml.css'
 
 const filterHtml = ({filterChange}) => {
     return (
@@ -9,36 +8,36 @@ const filterHtml = ({filterChange}) => {
 
             <details>
                 <summary className='summary'>Global</summary>
-                <ul>
+                <ul className='list'>
                     <li>
                         <details>
                             <summary className='country'>Country</summary>
-                            <ul>
+                            <ul className='list'>
                                 <li>
                                     <button className='haitiBtn' onClick={(e) => filterChange(1, "country")}>Haiti</button>
                                     <details>
 
                                     
                                     <summary>Department</summary>
-                                        <ul>
+                                        <ul className='list'>
                                             <li>
                                                 <details>
                                                     <summary>Artbonite</summary>
-                                                    <ul>
+                                                    <ul className='list'>
                                                         <li>
-                                                            <button onClick={(e) => filterChange(22)}>Deschapelle</button>
+                                                            <button onClick={(e) => filterChange(22,"community")}>Deschapelle</button>
                                                         </li>
                                                         <li>
-                                                            <button onClick={(e) => filterChange(36)}>Gonaïves</button>
+                                                            <button onClick={(e) => filterChange(36,"community")}>Gonaïves</button>
                                                         </li>
                                                         <li>
-                                                            <button onClick={(e) => filterChange(21)}>Jean Denis</button>
+                                                            <button onClick={(e) => filterChange(21,"community")}>Jean Denis</button>
                                                         </li>
                                                         <li>
-                                                            <button onClick={(e) => filterChange(23)}>Marmelade</button>
+                                                            <button onClick={(e) => filterChange(23,"community")}>Marmelade</button>
                                                         </li>
                                                         <li>
-                                                            <button onClick={(e) => filterChange(15)}>St.Michel</button>
+                                                            <button onClick={(e) => filterChange(15,"community")}>St.Michel</button>
                                                         </li>
                                                     </ul>
                                                 </details>
@@ -46,18 +45,18 @@ const filterHtml = ({filterChange}) => {
                                             <li>
                                                 <details>
                                                     <summary>Centre</summary>
-                                                    <ul>
+                                                    <ul className='list'>
                                                         <li>
-                                                            <button onClick={(e) => filterChange(13)}>Cerca-Carvajal</button>
+                                                            <button onClick={(e) => filterChange(13,"community")}>Cerca-Carvajal</button>
                                                         </li>
                                                         <li>
-                                                            <button onClick={(e) => filterChange(8)}>Cerca-la-Source</button>
+                                                            <button onClick={(e) => filterChange(8,"community")}>Cerca-la-Source</button>
                                                         </li>
                                                         <li>
-                                                            <button onClick={(e) => filterChange(12)}>Hinche</button>
+                                                            <button onClick={(e) => filterChange(12,"community")}>Hinche</button>
                                                         </li>
                                                         <li>
-                                                            <button onClick={(e) => filterChange(7)}>Thomassique</button>
+                                                            <button onClick={(e) => filterChange(7,"community")}>Thomassique</button>
                                                         </li>
                                                     </ul>
                                                 </details>
@@ -66,9 +65,9 @@ const filterHtml = ({filterChange}) => {
                                                 Grande'Anse
                                                 {/* <details>
                                                     <summary>Grand'Anse</summary>
-                                                    <ul>
+                                                    <ul className='list'>
                                                         <li>
-                                                            <button onClick={(e) => filterChange()}>ABCD</button>
+                                                            <button onClick={(e) => filterChange()},"community">ABCD</button>
                                                         </li> 
                                                     </ul>
                                                 </details> */}
@@ -77,9 +76,9 @@ const filterHtml = ({filterChange}) => {
                                                 Nippes
                                                 {/* <details>
                                                     <summary>Nippes</summary>
-                                                    <ul>
+                                                    <ul className='list'>
                                                         <li>
-                                                            <button onClick={(e) => filterChange()}>ABCD</button>
+                                                            <button onClick={(e) => filterChange()},"community">ABCD</button>
                                                         </li>
                                                     </ul> 
                                                 </details> */}
@@ -87,39 +86,39 @@ const filterHtml = ({filterChange}) => {
                                             <li>
                                                 <details>
                                                     <summary>Nord</summary>
-                                                    <ul>
+                                                    <ul className='list'>
                                                         <li>
-                                                            <button onClick={(e) => filterChange(37)}>Bahon</button>
+                                                            <button onClick={(e) => filterChange(37,"community")}>Bahon</button>
                                                         </li>
                                                         <li>
-                                                            <button onClick={(e) => filterChange(33)}>Bas-Limbé</button>
+                                                            <button onClick={(e) => filterChange(33,"community")}>Bas-Limbé</button>
                                                         </li>
                                                         <li>
-                                                            <button onClick={(e) => filterChange(19)}>Dondon</button>
+                                                            <button onClick={(e) => filterChange(19,"community")}>Dondon</button>
                                                         </li>
                                                         <li>
-                                                            <button onClick={(e) => filterChange(32)}>Grande Rivière du Nord</button>
+                                                            <button onClick={(e) => filterChange(32,"community")}>Grande Rivière du Nord</button>
                                                         </li>
                                                         <li>
-                                                            <button onClick={(e) => filterChange(11)}>Lavictoire</button>
+                                                            <button onClick={(e) => filterChange(11,"community")}>Lavictoire</button>
                                                         </li>
                                                         <li>
-                                                            <button onClick={(e) => filterChange(28)}>Limonade</button>
+                                                            <button onClick={(e) => filterChange(28,"community")}>Limonade</button>
                                                         </li>
                                                         <li>
-                                                            <button onClick={(e) => filterChange(10)}>Pignon</button>
+                                                            <button onClick={(e) => filterChange(10,"community")}>Pignon</button>
                                                         </li>
                                                         <li>
-                                                            <button onClick={(e) => filterChange(30)}>Plaine du Nord</button>
+                                                            <button onClick={(e) => filterChange(30,"community")}>Plaine du Nord</button>
                                                         </li>
                                                         <li>
-                                                            <button onClick={(e) => filterChange(29)}>Quartier-morin</button>
+                                                            <button onClick={(e) => filterChange(29,"community")}>Quartier-morin</button>
                                                         </li>
                                                         <li>
-                                                            <button onClick={(e) => filterChange(20)}>Ranquitte</button>
+                                                            <button onClick={(e) => filterChange(20,"community")}>Ranquitte</button>
                                                         </li>
                                                         <li>
-                                                            <button onClick={(e) => filterChange(14)}>St-Raphael</button>
+                                                            <button onClick={(e) => filterChange(14,"community")}>St-Raphael</button>
                                                         </li>
                                                     </ul>
                                                 </details>
@@ -127,26 +126,26 @@ const filterHtml = ({filterChange}) => {
                                             <li>
                                             <details>
                                                     <summary>Nord-Est</summary>
-                                                    <ul>
+                                                    <ul className='list'>
                                                         <li>
-                                                            <button onClick={(e) => filterChange(9)}>Mombin Crochus</button>
+                                                            <button onClick={(e) => filterChange(9,"community")}>Mombin Crochus</button>
                                                         </li>
                                                         <li>
-                                                            <button onClick={(e) => filterChange(34)}>Mont-Organisé</button>
+                                                            <button onClick={(e) => filterChange(34,"community")}>Mont-Organisé</button>
                                                         </li>
                                                         <li>
-                                                            <button onClick={(e) => filterChange(25)}>Ouanaminthe</button>
+                                                            <button onClick={(e) => filterChange(25,"community")}>Ouanaminthe</button>
                                                         </li>
                                                         <li>
-                                                            <button onClick={(e) => filterChange(35)}>Vallières</button>
+                                                            <button onClick={(e) => filterChange(35,"community")}>Vallières</button>
                                                         </li>
                                                     </ul>
                                                 </details>
                                             </li>
                                             <li> Norde-Ouest
-//                                                 <details>
+{/* //                                                 <details>
 //                                                     <summary>Norde-Ouest</summary>
-// //                                                     <ul>
+// //                                                     <ul className='list'>
 //                                                         <li>
 //                                                             <button onClick={(e) => filterChange()}>ABCD</button>
 //                                                         </li>
@@ -160,13 +159,13 @@ const filterHtml = ({filterChange}) => {
 //                                                             <button onClick={(e) => filterChange()}>ABCD</button>
 //                                                         </li>
 //                                                     </ul>
-//                                                 </details>
+//                                                 </details> */}
                                             </li>
                                             <li>
                                                 Ouest
                                                 {/* <details>
                                                     <summary>Ouest</summary>
-                                                    <ul>
+                                                    <ul className='list'>
                                                         <li>
                                                             <button onClick={(e) => filterChange()}>ABCD</button>
                                                         </li>
@@ -177,7 +176,7 @@ const filterHtml = ({filterChange}) => {
                                                 Sud
                                                 {/* <details>
                                                     <summary>Sud</summary>
-                                                    <ul>
+                                                    <ul className='list'>
                                                         <li>
                                                             <button onClick={(e) => filterChange()}>ABCD</button>
                                                         </li>
@@ -188,7 +187,7 @@ const filterHtml = ({filterChange}) => {
                                                 Sud-Est
                                                 {/* <details>
                                                     <summary>Sud-Est</summary>
-                                                    <ul>
+                                                    <ul className='list'>
                                                         <li>
                                                             <button onClick={(e) => filterChange()}>ABCD</button>
                                                         </li>
@@ -199,7 +198,7 @@ const filterHtml = ({filterChange}) => {
                                                 US Department
                                                 {/* <details>
                                                     <summary>US Department</summary>
-                                                    <ul>
+                                                    <ul className='list'>
                                                         <li>
                                                             <button onClick={(e) => filterChange()}>ABCD</button>
                                                         </li>

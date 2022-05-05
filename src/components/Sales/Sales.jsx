8a4d525386622046fc0c625340f456797346c453
BootsStribling <williamstriblingenterprises@gmail.com>
2022-05-04 22:59:32 -0500

    get data to render to pie-chart

--- src/components/Sales/Sales.jsx
+++ src/components/Sales/Sales.jsx
@@ -1,8 +1,38 @@
-import React from 'react'
+import {useState, useEffect} from 'react'
+import { PieChart } from 'react-minimal-pie-chart' 
+import * as salesService from '../../services/salesService'
 
-function Sales() {
+const Sales = () =>  {
+  const[loanSales, setLoanSales] = useState()
+  const[cashSales, setCashSales] = useState()
+  const[filter, setFilter] = useState()
+
+  useEffect(() => {
+    salesService.getTransactionTotal()
+    .then(res => {
+      setLoanSales(res[1])
+      setCashSales(res[2])
+    })
+  },[])
+
+  useEffect(() => {
+    salesService.getTransactionTotal()
+    .then(res => {
+      setLoanSales(res[1])
+      setCashSales(res[2])
+    })
+  },[filter])
+
   return (
-    <div>SALES</div>
+    <>
+      <PieChart
+      data={[
+        { title: 'Cash', value: loanSales, color: '#E38627' },
+        { title: 'Loan', value: cashSales, color: '#C13C37' },
+      ]}
+      />
+      <div>SALES</div>
+    </>
   )
 }
 
